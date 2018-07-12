3629d98d987b827b20c177302c25278143064b2e
rahularya198920 <36296566+rahularya198920@users.noreply.github.com>
2018-07-12 19:52:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def incidents():
     return render_template('incidents.html')
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 9090))
 	app.run(host='0.0.0.0', debug=True, port=port)
 
 
