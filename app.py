5e82dbb042b58229ef7f440540aa4fe94c1445b8
rahularya198920 <36296566+rahularya198920@users.noreply.github.com>
2018-07-12 18:49:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from flask import Flask,render_template,request
+import os
 
 import json
 app= Flask(__name__)
@@ -30,7 +31,8 @@ def signUp():
 def incidents():
     return render_template('incidents.html')
 
-if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
 
 
